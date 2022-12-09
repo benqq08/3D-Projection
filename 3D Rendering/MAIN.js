664290e776ec6cf47395a8d26687c941657907e6
benqq08 <114691041+benqq08@users.noreply.github.com>
2022-12-09 09:48:40 -0800

    Update MAIN.js

--- 3D Rendering/MAIN.js
+++ 3D Rendering/MAIN.js
@@ -1,13 +1,14 @@
+//hi :)
 
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
 var Cam = {
-    FOV: 70,
+    FOV: .5,
     X:0,
     Y:0,
     Z:0,
-    XDIR:0,
+    XDIR:Math.PI,
     YDIR:0,
     ZDIR:0,
 }
@@ -61,10 +62,23 @@ function lerp(a,b,c){
     return a+(b-a)*c
 }
 function drawTo(sx,sy,fx,fy){
-    const dist = (Math.abs(fx-sx)+Math.abs(fy-sy))/2
+    const dist = (Math.abs(fx-sx)+Math.abs(fy-sy))/6.8
     for (let i=0;i<dist;i++){
         context.fillRect(lerp(sx,fx,i/dist)+500,lerp(sy,fy,i/dist)+500,5,5)
     }
+}
+
+function drawTri(p1,p2,p3){
+
+}
+
+
+function fillTri(P1,P2,P3,xdir,ydir,zdir,dist){
+    let p1 = project(P1[0],P1[1],P1[2],P1[3],P1[4],P1[5],xdir,ydir,zdir,dist)
+    let p2 = project(P2[0],P2[1],P2[2],P2[3],P2[4],P2[5],xdir,ydir,zdir,dist)
+    let p3 = project(P3[0],P3[1],P3[2],P3[3],P3[4],P3[5],xdir,ydir,zdir,dist)
+
+    drawTri(p1,p2,p3)
 }
 
 function project(rX,rY,rZ,rX2,rY2,rZ2,xdir,ydir,zdir,dist){
@@ -75,15 +89,13 @@ function project(rX,rY,rZ,rX2,rY2,rZ2,xdir,ydir,zdir,dist){
     const Y2 = rY2 + Cam.Y
     const Z2 = rZ2 + Cam.Z
 
-
-
     let finlv1 = rot2(X,Y,Z,xdir,ydir,zdir)
-    let m1 = 240 / ((finlv1[2] + dist) * Math.tan(Cam.FOV/2))
+    let m1 = 240 / ((finlv1[2] + dist) * Math.abs(Cam.FOV/2))
     const RRx = finlv1[0]*m1
     const RRy = finlv1[1]*m1
 
     let finlv2 = rot2(X2,Y2,Z2,xdir,ydir,zdir)
-    let m2 = 240 / ((finlv2[2] + dist) * Math.tan(Cam.FOV/2))
+    let m2 = 240 / ((finlv2[2] + dist) * Math.abs(Cam.FOV/2))
 
     if (m2 < 0 || m1 < 0){
         return
@@ -92,27 +104,10 @@ function project(rX,rY,rZ,rX2,rY2,rZ2,xdir,ydir,zdir,dist){
     const RRx2 = finlv2[0]*m2
     const RRy2 = finlv2[1]*m2
 
-    drawTo(RRx,RRy,RRx2,RRy2)
+    return [RRx,RRy,RRx2,RRy2]
 
 
 }
-
-function voxel(xx,yy,zz,size){
-    project((-1)+xx,(-1)+yy,(-1)+zz,(1)+xx,(-1)+yy,(-1)+zz     ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(1)+yy,(-1)+zz,(1)+xx,(1)+yy,(-1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(1)+yy,(1)+zz,(1)+xx,(1)+yy,(1)+zz         ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(-1)+yy,(1)+zz,(1)+xx,(-1)+yy,(1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(-1)+yy,(-1)+zz,(-1)+xx,(1)+yy,(-1)+zz     ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((1)+xx,(-1)+yy,(-1)+zz,(1)+xx,(1)+yy,(-1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(-1)+yy,(1)+zz,(-1)+xx,(1)+yy,(1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((1)+xx,(-1)+yy,(1)+zz,(1)+xx,(1)+yy,(1)+zz         ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(-1)+yy,(-1)+zz,(-1)+xx,(-1)+yy,(1)+zz     ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((1)+xx,(-1)+yy,(-1)+zz,(1)+xx,(-1)+yy,(1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((1)+xx,(1)+yy,(-1)+zz,(1)+xx,(1)+yy,(1)+zz         ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-    project((-1)+xx,(1)+yy,(-1)+zz,(-1)+xx,(1)+yy,(1)+zz       ,Cam.XDIR,Cam.YDIR,Cam.ZDIR,size)
-}
-
-
 
 document.addEventListener("mousemove", () => {
     inputs.X = event.clientX/100
@@ -125,12 +120,7 @@ document.addEventListener("mouseup", () => {
     inputs.DOWN = false
 });
 
-/*
-canvas.requestPointerLock = canvas.requestPointerLock ||
-                            canvas.mozRequestPointerLock;
 
-canvas.requestPointerLock()
-*/
 document.addEventListener('keydown', function(e) {
     if (e.which === 87 ) {
         inputs.W=true
@@ -159,42 +149,42 @@ document.addEventListener('keyup', function(e) {
         inputs.D=false
     }
 });
-
-
 function loop() {
+
     context.fillStyle = `rgb(5,5,15)`
     context.fillRect(0,0,1000,1000)
     context.fillStyle = `rgb(200,200,205)`
 
+    Cam.XDIR += .011
+    Cam.YDIR += .02
+    Cam.ZDIR += .025
+    //voxel(0,0,0,5);
 
-    inputs.YSP = lerp(inputs.YSP,inputs.Y,.1)
-    inputs.XSP = lerp(inputs.XSP,inputs.X,.1)
-    if (inputs.DOWN==true){
-        inputs.ZSP = lerp(inputs.ZSP,inputs.X,.04)
-    }
     
-    if (inputs.W==true){
-        Cam.X += Math.cos(Cam.YDIR-90)/5
-        Cam.Y -= Math.sin(Cam.XDIR)/5
-    }
-    if (inputs.S==true){
-        Cam.X -= Math.cos(Cam.YDIR-90)/5
-        Cam.Y += Math.sin(Cam.XDIR)/5
-    }
+    fillTri([0,0,0,0,1,0],[0,1,0,1,1,0],[1,1,0,0,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+
+    fillTri([0,0,0,1,0,0],[1,0,0,1,1,0],[1,1,0,0,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+
+    fillTri([0,0,1,0,1,1],[0,1,1,1,1,1],[1,1,1,0,0,1],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+
+    fillTri([0,0,1,1,0,1],[1,0,1,1,1,1],[1,1,1,0,0,1],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+
+    fillTri([0,0,0,0,0,1],[0,0,1,0,1,1],[0,1,1,0,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+
+    fillTri([0,0,0,0,1,0],[0,1,0,0,1,1],[0,1,1,0,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
     
+    fillTri([1,0,0,1,1,1],[1,1,1,1,1,0],[1,1,0,1,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+    
+    fillTri([1,1,1,1,0,1],[1,0,1,1,0,0],[1,0,0,1,1,1],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+    
+    fillTri([0,1,0,1,1,0],[1,1,0,1,1,1],[1,1,1,0,1,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
 
-    Cam.YDIR = inputs.XSP
-    Cam.XDIR = inputs.YSP
-    Cam.ZDIR = inputs.ZSP
-    voxel(0,0,0,5);
+    fillTri([0,1,0,0,1,1],[0,1,0,1,1,1],[1,1,1,0,1,1],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
 
-    context.fillStyle = `rgb(100,220,100)`
-    project(0,.2,0,0,2,0,Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
-    context.fillStyle = `rgb(100,100,220)`
-    project(0,0,.2,0,0,2,Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
-    context.fillStyle = `rgb(220,100,100)`
-    project(.2,0,0,2,0,0,Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+    fillTri([0,0,0,1,0,0],[1,0,0,1,0,1],[1,0,1,0,0,0],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
 
+    fillTri([0,0,0,0,0,1],[0,0,0,1,0,1],[1,0,1,0,0,1],Cam.XDIR,Cam.YDIR,Cam.ZDIR,5)
+    
     requestAnimationFrame(loop);
 };
 
